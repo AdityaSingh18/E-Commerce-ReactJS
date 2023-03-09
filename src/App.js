1d0dec5ddb6c1e7bb30b9b8558e37a4cfbe59306
Aditya <aditya18.mail@gmail.com>
2023-03-09 20:45:12 +0530

    Building Blocks of Ecommerce

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "react-bootstrap/Button";
+import "./App.css";
+import HeaderSite from "./components/UI/HeaderSite";
+import ItemList from "./components/Store/ItemList";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Store from "./components/Store/Store";
 
 function App() {
+  const productsArr = [
+    {
+      title: "Colors",
+
+      price: 100,
+
+      imageUrl:
+        "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
+    },
+
+    {
+      title: "Black and white Colors",
+
+      price: 50,
+
+      imageUrl:
+        "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
+    },
+
+    {
+      title: "Yellow and Black Colors",
+
+      price: 70,
+
+      imageUrl:
+        "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
+    },
+
+    {
+      title: "Blue Color",
+
+      price: 100,
+
+      imageUrl:
+        "https://prasadyash2411.github.io/ecom-website/img/Album%204.png",
+    },
+  ];
+  
+  const arr = productsArr.map((item)=>{
+return(
+    <div>
+      {item}
+    </div>
+)
+  })
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="App">
+      <HeaderSite />
+      {productsArr.map((item) => (
+        <Store key={item.title} item={item} />
+      ))}
     </div>
   );
 }
