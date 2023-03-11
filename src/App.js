1b1dfec963857f52bbbc7cc80b16d41074d60611
Aditya <aditya18.mail@gmail.com>
2023-03-11 11:13:36 +0530

    Implementing React Router

--- src/App.js
+++ src/App.js
@@ -4,64 +4,27 @@ import HeaderSite from "./components/UI/HeaderSite";
 
 import Store from "./components/StorePage/Store";
 import CartProvider from "./components/store/CartProvider";
+import {createBrowserRouter,RouterProvider} from 'react-router-dom';
+import Home from './components/Home/Home';
+import About from './components/About/About'
+import StoreData from "./components/StorePage/StoreData";
 
+ const router = createBrowserRouter([
+  {path:'/',element:<Home/>},
+  {path:'/store',element:<StoreData/>},
+  {path:'/about',element:<About/>}
+])
 
 function App() {
-  const productsArr = [
-    {
-      title: "Colors",
 
-      price: 100,
 
-      imageUrl:
-        "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
-    },
-
-    {
-      title: "Black and white Colors",
-
-      price: 50,
-
-      imageUrl:
-        "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
-    },
-
-    {
-      title: "Yellow and Black Colors",
-
-      price: 70,
-
-      imageUrl:
-        "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
-    },
-
-    {
-      title: "Blue Color",
-
-      price: 100,
-
-      imageUrl:
-        "https://prasadyash2411.github.io/ecom-website/img/Album%204.png",
-    },
-  ];
-  
-  const arr = productsArr.map((item)=>{
-return(
-    <div>
-      {item}
-    </div>
-)
-  })
   
   return (
     <CartProvider>
-    
-      <HeaderSite />
-      {productsArr.map((item) => (
-        <Store key={item.title} item={item} />
-      ))}
+  
+<RouterProvider router={router}/>
+      
     </CartProvider>
   );
-}
-
+      }
 export default App;
